cc0d973f8c883ec653266893b7c3de819114ebda
garderony <y.garderon@gmail.com>
2016-11-22 22:24:18 +0100

    Update gpio.exec.js

--- gpio.exec.js
+++ gpio.exec.js
@@ -2,6 +2,11 @@
 var Gpio = require('onoff').Gpio;
  var led = new Gpio(4,'out');
 
-module.exports = function exec() {
-            led.writeSync(0);     
+module.exports = function exec(params) {
+ if(params === 0){           
+ led.writeSync(0); 
+ }
+ else{
+ led.writeSync(1);
+ }
 };
